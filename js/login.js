698a40f176246fc6075f43ad7d514001d0d4e305
romina2911 <romina.hp@gmail.com>
2017-12-29 03:12:56 -0500

    habilitanto y deshabilitando boton

--- js/login.js
+++ js/login.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
     //extrayendo elementos 
     var $email = $('#inputEmail3');
     var $password = $('#inputPassword3');
+    var $checkbox =$('.checklog');
+    var $submit = $('button[type=submit]') 
+    console.log($checkbox);
+    //verificando valores para habilitar boton
+    var verifyEmail = false;
+    var verifyPassword=false;
+    var verifyCheck = false;
+
+
+
    //asociando eventos a los elementos seleccionados
    //Validando email
    $email.on('input', function (event){
@@ -9,9 +19,10 @@ $(document).ready(function(){
        var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
        console.log(PATERNEMAIL.test($(this).val()));
        if(PATERNEMAIL.test($(this).val())){
-           alert('Este correo es valido')
+        var verifyEmail = true;
+        activeButton();
        }else{
-           console.log('Ingrese un correo valido');
+        desactiveButton();
        }
     });
     //validando password
@@ -22,6 +33,26 @@ $(document).ready(function(){
         console.log('contraseña valida');
     }
    });
+
+   //asociar evento a checkbox
+   $checkbox.on('click', function(event){
+       if(event.target.checked){
+           console.log('acepta');
+       }else{
+        console.log('no acepta');
+       }
+   });
+
+   //creando función active button
+   function activeButton(){
+           $submit.attr('disabled', false);
+    
+   }
+
+   function desactiveButton() {
+        $submit.attr('disabled', true);
+     }
+
 });
 
  
