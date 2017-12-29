21dd6a671fb5cbbee961cee0ea5af767d382e5bf
romina2911 <romina.hp@gmail.com>
2017-12-29 01:25:54 -0500

    validacion de correo

--- js/login.js
+++ js/login.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
     //extrayendo elementos 
     var $email = $('#inputPassword3');
    //asociando eventos a los elementos seleccionados
-   $email.on('input', function (){
-
-
-
-   });
-    
-    
-    
-    
-    
-    
-    
-    
+   $email.on('input', function (event){
+       $(this).val();
+       var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+       console.log(PATERNEMAIL.test($(this).val()));
+   
     });
+   
+   $password.on('input', function(event) {
+       if($(this).val().length >= 6){
+           alert('cumple');
+       }else{
+           alert('no cumple');
+       }
+   
+     });
+   });
+
+ 
