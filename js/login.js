a9b58daf5a52842b802dc2451b730e3731b3238f
romina2911 <romina.hp@gmail.com>
2017-12-29 01:58:14 -0500

    validacion de password

--- js/login.js
+++ js/login.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
     //extrayendo elementos 
     var $email = $('#inputEmail3');
+    var $password = $('#inputPassword3');
    //asociando eventos a los elementos seleccionados
+   //Validando email
    $email.on('input', function (event){
        $(this).val();
        var PATERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
        console.log(PATERNEMAIL.test($(this).val()));
-   
-    });
-    var $password = $('#inputPassword3');
-   $password.on('input', function(event) {
-       if($(this).val().length >= 6){
-           alert('cumple');
+       if(PATERNEMAIL.test($(this).val())){
+           alert('Este correo es valido')
        }else{
-           alert('no cumple');
+           console.log('Ingrese un correo valido');
        }
-   
-     });
+    });
+    //validando password
+   $password.on('input', function(event) {
+    if($(this).val().length <= 6){
+        console.log('la contraseña debe tener minimo 6 caracteres');
+    }else{
+        console.log('contraseña valida');
+    }
    });
+});
 
  
